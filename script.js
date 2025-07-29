db6ece6cf11b7daade01337db7f232805a255e82
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-07-29 23:24:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const taskList = document.getElementById('task-list');
  
   const removeBtn = document.createElement('button');
   removeBtn.textContent = 'Remove';
-  removeBtn.className = 'remove-btn';
+  removeBtn.classList.add('remove-btn');
 
   
   removeBtn.onclick = () => {
