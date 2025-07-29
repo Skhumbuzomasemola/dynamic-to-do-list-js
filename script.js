9396b807d111788127176d93e32b02f007429dc6
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-07-29 23:22:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const addButton = document.getElementById('addTask');
-  const taskInput = document.getElementById('task-input');
-  const taskList = document.getElementById('task-list');
-
+ const addButton = document.getElementById('add-task-btn');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
   function addTask() {
     const taskText = taskInput.value.trim(); // retrieve and trim input value
 
